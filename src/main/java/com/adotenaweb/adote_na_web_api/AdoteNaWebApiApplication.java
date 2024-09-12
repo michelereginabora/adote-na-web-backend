bb4cffcf06ae00bba94e6fd9266d57f10f370bc2
Michele Regina Bora <miregina@live.com>
2024-09-12 15:01:41 -0300

    initial setup

---
+++ src/main/java/com/adotenaweb/adote_na_web_api/AdoteNaWebApiApplication.java
@@ -0,0 +1,13 @@
+package com.adotenaweb.adote_na_web_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdoteNaWebApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdoteNaWebApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/adotenaweb/adote_na_web_api/AdoteNaWebApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adotenaweb.adote_na_web_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdoteNaWebApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
