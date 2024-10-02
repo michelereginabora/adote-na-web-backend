fbedfd897a7c02682ad75e783a3774bcd6c33ba6
Michele Regina Bora <miregina@live.com>
2024-10-02 18:03:57 -0300

    feat: implement JWT authentication and security

--- src/main/java/com/adotenaweb/adotenawebapi/controllers/auth/AuthenticationController.java
+++ src/main/java/com/adotenaweb/adotenawebapi/controllers/auth/AuthenticationController.java
@@ -1,7 +1,10 @@
 package com.adotenaweb.adotenawebapi.controllers.auth;
 
 
+import com.adotenaweb.adotenawebapi.models.dtos.auth.AuthResponseDTO;
 import com.adotenaweb.adotenawebapi.models.dtos.auth.AuthenticationDTO;
+import com.adotenaweb.adotenawebapi.models.entities.user.User;
+import com.adotenaweb.adotenawebapi.security.TokenService;
 import com.adotenaweb.adotenawebapi.services.auth.AuthenticationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -14,9 +17,14 @@ public class AuthenticationController {
     @Autowired
     private AuthenticationService authenticationService;
 
+    @Autowired
+    TokenService tokenService;
+
     @PostMapping("/login")
     public ResponseEntity<?> loginUser(@RequestBody AuthenticationDTO authDTO) {
-        authenticationService.authenticateUser(authDTO);
-        return ResponseEntity.ok("Login successful");
+        User auth = authenticationService.authenticateUser(authDTO);
+
+        var token = tokenService.generateToken(auth);
+        return ResponseEntity.ok(new AuthResponseDTO(token));
     }
 }
--- src/main/java/com/adotenaweb/adotenawebapi/services/auth/AuthenticationService.java
+++ src/main/java/com/adotenaweb/adotenawebapi/services/auth/AuthenticationService.java
@@ -5,6 +5,7 @@ import com.adotenaweb.adotenawebapi.models.dtos.auth.AuthenticationDTO;
 import com.adotenaweb.adotenawebapi.models.entities.user.User;
 import com.adotenaweb.adotenawebapi.repositories.user.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -17,12 +18,14 @@ public class AuthenticationService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
-    public void authenticateUser(AuthenticationDTO authDTO) {
-        User user = userRepository.findByPhone(authDTO.getPhone())
+    public User authenticateUser(AuthenticationDTO authDTO) {
+        UserDetails userDetails = userRepository.findByPhone(authDTO.getPhone())
                 .orElseThrow(() -> new InvalidCredentialsException("User not found"));
 
-        if (!passwordEncoder.matches(authDTO.getPassword(), user.getPassword())) {
+        if (!passwordEncoder.matches(authDTO.getPassword(), userDetails.getPassword())) {
             throw new InvalidCredentialsException("Invalid password");
         }
+
+        return (User) userDetails;
     }
 }
